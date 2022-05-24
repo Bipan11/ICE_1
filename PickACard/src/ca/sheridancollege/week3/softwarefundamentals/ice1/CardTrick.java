f982c56f23a636ace93342feb732c6b24b45dc2c
ADMIN <ADMIN@DESKTOP-2481SMM>
2022-05-24 19:51:32 -0400

    Added two new classes to a branch.

---
+++ PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import java.util.Scanner;
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author Bipandeep Kaur
+ * Student ID-991645867
+ * Date-May 24,2022
+ */
+public class CardTrick {
+    
+    public static void main(String[] args)
+    {
+        Scanner some = new Scanner(System.in);
+        Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            
+            c.setValue(c.userValue());
+            //c.setValue(insert call to random number generator here)
+            c.setSuit(Card.SUITS[c.userSuit()]);
+            magicHand[i]=c;
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+        
+       
+        for(Card result:magicHand){
+            System.out.println(result.getSuit());
+            System.out.println(result.getValue());
+        }
+        System.out.println("Enter the value");
+        
+        int Value1 = some.nextInt();
+        System.out.println("Enter for the card suit");
+        
+        int Suit1  = some.nextInt();
+        System.out.println("You card is : " +Value1+ "of "+Suit1);
+        //insert code to ask the user for Card value and suit, create their card
+        
+        
+        // and search magicHand here
+        for(Card result:magicHand){
+        if(Value1 == result.getValue()&& Suit1 ==result.getValue()){
+            System.out.println("Magic is Succesfull");
+        }
+        }
+        //Then report the result here
+    }
+
+
+    
+}
---
+++ PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/newMagic.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+/**
+ *
+ * @author ADMIN
+ */
+class newMagic {
+
+    static int getValue() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
